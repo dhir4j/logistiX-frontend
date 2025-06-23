7e68addbbdd1b1732ced2a5148c0f199635069e0
dhir4j <djkapse007@gmail.com>
2025-06-23 03:05:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,15 +8,11 @@ export const metadata: Metadata = {
   title: 'Shed Load Overseas',
   description: 'Courier and shipment management solutions by Shed Load Overseas.',
   icons: {
-<<<<<<< HEAD
     icon: {
       url: "/favicon.ico",
       type: "image/x-icon",
     },
     shortcut: { url: "/favicon.ico", type: "image/x-icon" },
-=======
-    icon: '/favicon.ico', 
->>>>>>> 9b75c106224b64d7f36b5ea78401b727420310ca
   }
 };
 
